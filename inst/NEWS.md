2fc0aeb092b6f55d799d74fc791431377be5dd0e
tomoyukif <f.tomoyuki@okayama-u.ac.jp>
2021-11-24 18:12:10 +0900

    fix test

--- inst/NEWS.md
+++ inst/NEWS.md
@@ -5,5 +5,5 @@ Changes in version 0.99.16 (2021-10-20)
 + Fix a bug that probabilities can be 0 for all genotypes when mismap rate is 1
 
 
-Changes in version 0.99.24 (2021-11-24)
+Changes in version 0.99.25 (2021-11-24)
 + Reformatted the scripts and the vignette to meet Bioconductor's instruction.
--- tests/testthat/test_calcReadStats.R
+++ tests/testthat/test_calcReadStats.R
@@ -18,13 +18,13 @@ ref[ref == 0] <- NA
 alt[alt == 0] <- NA
 
 test_that("Mean reference read", {
-    expect_equal(getMeanReadRef(gds, "snp"), 
+    expect_equal(getMeanReadRef(gds, "snp"),
                  colMeans(ref, na.rm = TRUE),
                  ignore_attr = TRUE)
     expect_equal(getMeanReadRef(gds, "scan"),
                  rowMeans(ref, na.rm = TRUE),
                  ignore_attr = TRUE)
-    
+
     valid_snp <- sample(c(TRUE, FALSE), nsnp(gds), TRUE)
     gds <- setValidSnp(gds, valid_snp)
     valid_scan <- sample(c(TRUE, FALSE), nscan(gds), TRUE)
@@ -37,26 +37,26 @@ test_that("Mean reference read", {
     tot <- rowSums(dp)
     ref <- ref / tot * 10^6
     alt <- alt / tot * 10^6
-    
+
     valid_ref <- ref[valid_scan, valid_snp]
     valid_ref[valid_ref == 0] <- NA
-    expect_equal(getMeanReadRef(gds, "snp", valid = TRUE), 
+    expect_equal(getMeanReadRef(gds, "snp", valid = TRUE),
                  colMeans(valid_ref, na.rm = TRUE),
                  ignore_attr = TRUE)
-    
+
     expect_equal(getMeanReadRef(gds, "scan", valid = TRUE),
                  rowMeans(valid_ref, na.rm = TRUE),
                  ignore_attr = TRUE)
 })
 
 test_that("Mean alternative read", {
-    expect_equal(getMeanReadAlt(gds, "snp"), 
+    expect_equal(getMeanReadAlt(gds, "snp"),
                  colMeans(alt, na.rm = TRUE),
                  ignore_attr = TRUE)
     expect_equal(getMeanReadAlt(gds, "scan"),
                  rowMeans(alt, na.rm = TRUE),
                  ignore_attr = TRUE)
-    
+
     valid_snp <- sample(c(TRUE, FALSE), nsnp(gds), TRUE)
     gds <- setValidSnp(gds, valid_snp)
     valid_scan <- sample(c(TRUE, FALSE), nscan(gds), TRUE)
@@ -69,26 +69,26 @@ test_that("Mean alternative read", {
     tot <- rowSums(dp)
     ref <- ref / tot * 10^6
     alt <- alt / tot * 10^6
-    
+
     valid_alt <- alt[valid_scan, valid_snp]
     valid_alt[valid_alt == 0] <- NA
-    expect_equal(getMeanReadAlt(gds, "snp", valid = TRUE), 
+    expect_equal(getMeanReadAlt(gds, "snp", valid = TRUE),
                  colMeans(valid_alt, na.rm = TRUE),
                  ignore_attr = TRUE)
-    
+
     expect_equal(getMeanReadAlt(gds, "scan", valid = TRUE),
                  rowMeans(valid_alt, na.rm = TRUE),
                  ignore_attr = TRUE)
 })
 
 test_that("SD of reference read", {
-    expect_equal(getSDReadRef(gds, "snp"), 
-                 apply(ref, 2, sd, na.rm = TRUE),
+    expect_equal(round(getSDReadRef(gds, "snp")),
+                 round(apply(ref, 2, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    expect_equal(getSDReadRef(gds, "scan"),
-                 apply(ref, 1, sd, na.rm = TRUE),
+    expect_equal(round(getSDReadRef(gds, "scan")),
+                 round(apply(ref, 1, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
+
     valid_snp <- sample(c(TRUE, FALSE), nsnp(gds), TRUE)
     gds <- setValidSnp(gds, valid_snp)
     valid_scan <- sample(c(TRUE, FALSE), nscan(gds), TRUE)
@@ -101,26 +101,26 @@ test_that("SD of reference read", {
     tot <- rowSums(dp)
     ref <- ref / tot * 10^6
     alt <- alt / tot * 10^6
-    
+
     valid_ref <- ref[valid_scan, valid_snp]
     valid_ref[valid_ref == 0] <- NA
-    expect_equal(getSDReadRef(gds, "snp", valid = TRUE), 
-                 apply(valid_ref, 2, sd, na.rm = TRUE),
+    expect_equal(round(getSDReadRef(gds, "snp", valid = TRUE)),
+                 round(apply(valid_ref, 2, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
-    expect_equal(getSDReadRef(gds, "scan", valid = TRUE),
-                 apply(valid_ref, 1, sd, na.rm = TRUE),
+
+    expect_equal(round(getSDReadRef(gds, "scan", valid = TRUE)),
+                 round(apply(valid_ref, 1, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
 })
 
 test_that("SD of alternative read", {
-    expect_equal(getSDReadAlt(gds, "snp"), 
-                 apply(alt, 2, sd, na.rm = TRUE),
+    expect_equal(round(getSDReadAlt(gds, "snp")),
+                 round(apply(alt, 2, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    expect_equal(getSDReadAlt(gds, "scan"),
-                 apply(alt, 1, sd, na.rm = TRUE),
+    expect_equal(round(getSDReadAlt(gds, "scan")),
+                 round(apply(alt, 1, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
+
     valid_snp <- sample(c(TRUE, FALSE), nsnp(gds), TRUE)
     gds <- setValidSnp(gds, valid_snp)
     valid_scan <- sample(c(TRUE, FALSE), nscan(gds), TRUE)
@@ -133,26 +133,26 @@ test_that("SD of alternative read", {
     tot <- rowSums(dp)
     ref <- ref / tot * 10^6
     alt <- alt / tot * 10^6
-    
+
     valid_alt <- alt[valid_scan, valid_snp]
     valid_alt[valid_alt == 0] <- NA
-    expect_equal(getSDReadAlt(gds, "snp", valid = TRUE), 
-                 apply(valid_alt, 2, sd, na.rm = TRUE),
+    expect_equal(round(getSDReadAlt(gds, "snp", valid = TRUE)),
+                 round(apply(valid_alt, 2, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
-    expect_equal(getSDReadAlt(gds, "scan", valid = TRUE),
-                 apply(valid_alt, 1, sd, na.rm = TRUE),
+
+    expect_equal(round(getSDReadAlt(gds, "scan", valid = TRUE)),
+                 round(apply(valid_alt, 1, sd, na.rm = TRUE)),
                  ignore_attr = TRUE)
 })
 
 test_that("Quantile of reference read", {
-    expect_equal(round(getQtileReadRef(gds, "snp")), 
+    expect_equal(round(getQtileReadRef(gds, "snp")),
                  round(apply(ref, 2, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
     expect_equal(round(getQtileReadRef(gds, "scan")),
                  round(apply(ref, 1, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
+
     valid_snp <- sample(c(TRUE, FALSE), nsnp(gds), TRUE)
     gds <- setValidSnp(gds, valid_snp)
     valid_scan <- sample(c(TRUE, FALSE), nscan(gds), TRUE)
@@ -165,13 +165,13 @@ test_that("Quantile of reference read", {
     tot <- rowSums(dp)
     ref <- ref / tot * 10^6
     alt <- alt / tot * 10^6
-    
+
     valid_ref <- ref[valid_scan, valid_snp]
     valid_ref[valid_ref == 0] <- NA
-    expect_equal(round(getQtileReadRef(gds, "snp", valid = TRUE)), 
+    expect_equal(round(getQtileReadRef(gds, "snp", valid = TRUE)),
                  round(apply(valid_ref, 2, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
+
     expect_equal(round(getQtileReadRef(gds, "scan", valid = TRUE)),
                  round(apply(valid_ref, 1, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
@@ -179,13 +179,13 @@ test_that("Quantile of reference read", {
 
 
 test_that("Quantile of alternative read", {
-    expect_equal(round(getQtileReadAlt(gds, "snp")), 
+    expect_equal(round(getQtileReadAlt(gds, "snp")),
                  round(apply(alt, 2, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
     expect_equal(round(getQtileReadAlt(gds, "scan")),
                  round(apply(alt, 1, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
+
     valid_snp <- sample(c(TRUE, FALSE), nsnp(gds), TRUE)
     gds <- setValidSnp(gds, valid_snp)
     valid_scan <- sample(c(TRUE, FALSE), nscan(gds), TRUE)
@@ -198,13 +198,13 @@ test_that("Quantile of alternative read", {
     tot <- rowSums(dp)
     ref <- ref / tot * 10^6
     alt <- alt / tot * 10^6
-    
+
     valid_alt <- alt[valid_scan, valid_snp]
     valid_alt[valid_alt == 0] <- NA
-    expect_equal(round(getQtileReadAlt(gds, "snp", valid = TRUE)), 
+    expect_equal(round(getQtileReadAlt(gds, "snp", valid = TRUE)),
                  round(apply(valid_alt, 2, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
-    
+
     expect_equal(round(getQtileReadAlt(gds, "scan", valid = TRUE)),
                  round(apply(valid_alt, 1, quantile, probs = q, na.rm = TRUE)),
                  ignore_attr = TRUE)
