923c6d958a65addf303fe68de9f67f2a390210a0
tomoyukif <f.tomoyuki@okayama-u.ac.jp>
2023-05-11 17:40:32 +0900

    version bump

--- R/Methods-GbsrGenotypeData.R
+++ R/Methods-GbsrGenotypeData.R
@@ -1299,7 +1299,7 @@ setMethod("setCallFilter",
     } else if(!greater & !equal){
         out <- x < y
     }
-    out[is.na(out)] <- FALSE
+    out[is.na(out)] <- TRUE
     return(out)
 }
 
@@ -1640,10 +1640,8 @@ setMethod("setInfoFilter",
     info_gdsn <- ls.gdsn(index.gdsn(object, "annotation/info"))
     if(filt_list$mq != 0){
         if("MQ" %in% info_gdsn){
-            filt <- .calcSubFilter(getInfo(object, "MQ"),
+            v <- v & .calcSubFilter(getInfo(object, "MQ"),
                                    filt_list$mq, 0, TRUE, TRUE)
-            filt[is.na(filt)] <- TRUE
-            v <- v & filt
         } else {
             warning("Filtering on MQ was specified but no MQ in the GDS.")
         }
@@ -1652,10 +1650,8 @@ setMethod("setInfoFilter",
     ## FS
     if(filt_list$fs != Inf){
         if("FS" %in% info_gdsn){
-            filt <- .calcSubFilter(getInfo(object, "FS"),
+            v <- v & .calcSubFilter(getInfo(object, "FS"),
                                    filt_list$fs, Inf, FALSE, TRUE)
-            filt[is.na(filt)] <- TRUE
-            v <- v & filt
         } else {
             warning("Filtering on FS was specified but no FS in the GDS.")
         }
@@ -1664,10 +1660,8 @@ setMethod("setInfoFilter",
     ## QD
     if(filt_list$qd != 0){
         if("QD" %in% info_gdsn){
-            filt <- .calcSubFilter(getInfo(object, "QD"),
+            v <- v & .calcSubFilter(getInfo(object, "QD"),
                                    filt_list$qd, 0, TRUE, TRUE)
-            filt[is.na(filt)] <- TRUE
-            v <- v & filt
         } else {
             warning("Filtering on QD was specified but no QD in the GDS.")
         }
@@ -1676,10 +1670,8 @@ setMethod("setInfoFilter",
     ## SOR
     if(filt_list$sor != Inf){
         if("SOR" %in% info_gdsn){
-            filt <- .calcSubFilter(getInfo(object, "SOR"),
+            v <- v & .calcSubFilter(getInfo(object, "SOR"),
                                    filt_list$sor, Inf, FALSE, TRUE)
-            filt[is.na(filt)] <- TRUE
-            v <- v & filt
         } else {
             warning("Filtering on SOR was specified but no SOR in the GDS.")
         }
@@ -1688,10 +1680,8 @@ setMethod("setInfoFilter",
     ## MQRankSum
     if(any(filt_list$mqranksum != c(-Inf, Inf))){
         if("MQRankSum" %in% info_gdsn){
-            filt <- .calcSubFilter(getInfo(object, "MQRankSum"),
+            v <- v & .calcSubFilter(getInfo(object, "MQRankSum"),
                                    filt_list$mqranksum, c(-Inf, Inf), TRUE, TRUE)
-            filt[is.na(filt)] <- TRUE
-            v <- v & filt
         } else {
             warning("Filtering on MQRankSum was specified but no MQRankSum in the GDS.")
         }
@@ -1700,11 +1690,9 @@ setMethod("setInfoFilter",
     ## Alternative allele read count
     if(any(filt_list$readposranksum != c(-Inf, Inf))){
         if("ReadPosRankSum" %in% info_gdsn){
-            filt <- .calcSubFilter(getInfo(object,
+            v <- v & .calcSubFilter(getInfo(object,
                                            "ReadPosRankSum"),
                                    filt_list$readposranksum, c(-Inf, Inf), TRUE, TRUE)
-            filt[is.na(filt)] <- TRUE
-            v <- v & filt
         } else {
             warning("Filtering on ReadPosRankSum was specified but no ReadPosRankSum in the GDS.")
         }
@@ -1713,10 +1701,8 @@ setMethod("setInfoFilter",
     ## Total read count
     if(any(filt_list$baseqranksum != c(-Inf, Inf))){
         if("BaseQRankSum" %in% info_gdsn){
-            filt <- .calcSubFilter(getInfo(object, "BaseQRankSum"),
+            v <- v & .calcSubFilter(getInfo(object, "BaseQRankSum"),
                                    filt_list$baseqranksum, c(-Inf, Inf), TRUE, TRUE)
-            filt[is.na(filt)] <- TRUE
-            v <- v & filt
         } else {
             warning("Filtering on BaseQRankSum was specified but no BaseQRankSum in the GDS.")
         }
